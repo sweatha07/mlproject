cb85f25cacf807b4043cc8c57742b0614721a6f2
Sweatha Mohan <sweathasm.mohan07@gmail.com>
2023-12-29 22:42:49 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail):
     _, _, exc_tb = error_detail
@@ -16,3 +17,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
